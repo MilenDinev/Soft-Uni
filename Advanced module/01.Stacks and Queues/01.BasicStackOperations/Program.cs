47cddb80713da247384a3dfab61fb8c6bd1cc651
Milen Dinev <dinev.milen@gmail.com>
2020-05-19 12:47:42 +0300

    Initial commit

---
+++ Advanced module/01.Stacks and Queues/01.BasicStackOperations/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.BasicStackOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersArgs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int n = numbersArgs[0];
+            int s = numbersArgs[1];
+            int x = numbersArgs[2];
+
+            int[] numbersArr = new int[n];
+
+            numbersArr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> numbers = new Stack<int>(numbersArr);
+
+
+            for (int i = 0; i < s; i++)
+            {
+                if (numbers.Count > 0)
+                {
+                    numbers.Pop();
+                }
+
+            }
+
+            if (numbers.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+
+            else if (numbers.Count > 0)
+            {
+                Console.WriteLine(numbers.Min());
+            }
+
+            else if (numbers.Count == 0)
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
---
+++ Advanced module/01.Stacks and Queues/10.Crossroads/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+
+namespace _10.Crossroads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int greenLightDuration = int.Parse(Console.ReadLine());
+            int freeWindow = int.Parse(Console.ReadLine());
+            var carsToPass = new Queue<string>();
+            var passedCars = new Stack<string>();
+
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                if (command != "green")
+                {
+                    carsToPass.Enqueue(command);
+                }
+
+                else 
+                {
+                    int greenLight = greenLightDuration;
+                    int freeW = freeWindow;
+                    int counter = carsToPass.Count;
+
+                    for (int i = 0; i < counter; i++)
+                    {
+                        if (carsToPass.Any())
+                        {
+                            string currentCar = carsToPass.Peek();
+
+                            if (greenLight >= currentCar.Length)
+                            {
+                                greenLight -= currentCar.Length;
+                                passedCars.Push(carsToPass.Dequeue());
+                            }
+
+                            else if (greenLight < currentCar.Length)
+                            {
+                                int timeLeft = greenLight + freeW;
+
+                                if (greenLight <= 0)
+                                {
+                                    continue;
+                                }
+
+                                else if (timeLeft > 0 && timeLeft >= currentCar.Length)
+                                {
+                                    timeLeft -= currentCar.Length;
+                                    passedCars.Push(carsToPass.Dequeue());
+                                    greenLight = 0;
+                                    freeW = 0;
+                                }
+                                else if (timeLeft > 0 && timeLeft < currentCar.Length)
+                                {
+                                    Console.WriteLine("A crash happened!");
+                                    int hit = timeLeft;
+                                    Console.WriteLine($"{currentCar} was hit at {currentCar[hit]}.");
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine("Everyone is safe.");
+            Console.WriteLine($"{passedCars.Count} total cars passed the crossroads.");
+        }
+    }
+}

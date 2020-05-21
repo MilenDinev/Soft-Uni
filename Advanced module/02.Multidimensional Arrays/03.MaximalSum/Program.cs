44a6231006246e10bcb3cfcdf910f9457ea91638
Milen Dinev <dinev.milen@gmail.com>
2020-05-21 19:34:04 +0300

    Initial commit

--- Advanced module/02.Multidimensional Arrays/03.MaximalSum/Program.cs
+++ Advanced module/02.Multidimensional Arrays/03.MaximalSum/Program.cs
@@ -15,10 +15,11 @@ namespace _03.MaximalSum
             int cols = dimensions[1];
             int[,] matrix = new int[rows, cols];
 
-            for (int row = 0; row < matrix.Length; row++)
+            for (int row = 0; row <rows; row++)
             {
                 int[] currentRow = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
                     .Select(int.Parse).ToArray();
+
                 for (int col = 0; col < cols; col++)
                 {
                     matrix[row, col] = currentRow[col];
@@ -29,6 +30,36 @@ namespace _03.MaximalSum
             int rowStartIndex = 0;
             int colStartIndex = 0;
 
+
+            for (int row = 0; row < rows - 2 ; row++)
+            {
+
+                for (int col = 0; col < cols - 2; col++)
+                {
+                    int sum = matrix[row, col] + matrix[row, col + 1] + matrix[row, col + 2]
+                        + matrix[row + 1, col] + matrix[row + 1, col + 1] + matrix[row + 1, col + 2]
+                        + matrix[row + 2, col] + matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
+
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        rowStartIndex = row;
+                        colStartIndex = col;
+                    }
+                }
+
+
+            }
+
+            Console.WriteLine($"Sum = {maxSum}");
+            for (int row = rowStartIndex; row <= rowStartIndex + 2; row++)
+            {
+                for (int col = colStartIndex; col <= colStartIndex + 2; col++)
+                {
+                    Console.Write($"{matrix[row, col]} ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }

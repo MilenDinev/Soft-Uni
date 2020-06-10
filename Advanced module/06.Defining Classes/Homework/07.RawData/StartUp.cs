2bc7bd0a26d51bd2eb045b44d33b339f9495710e
Milen Dinev <dinev.milen@gmail.com>
2020-06-10 17:11:59 +0300

    Initial commit

---
+++ Advanced module/06.Defining Classes/Homework/07.RawData/StartUp.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.RawData
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+
+            int counter = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < counter; i++)
+            {
+                string[] inputArgs = Console.ReadLine().Split().ToArray();
+
+                string carModel = inputArgs[0];
+
+
+                int engineSpeed = int.Parse(inputArgs[1]);
+                int enginePower = int.Parse(inputArgs[2]);
+
+
+
+                int cargoWeight = int.Parse(inputArgs[3]);
+                string type = inputArgs[4];
+
+                Tire[] tires = new Tire[4];
+                int tiresCounter = 0;
+
+                for (int tireIndex = 5; tireIndex < inputArgs.Length; tireIndex += 2)
+                {
+                    double currentTirePressure = double.Parse(inputArgs[tireIndex]);
+                    int currentTireAge = int.Parse(inputArgs[tireIndex + 1]);
+
+                    Tire tire = new Tire(currentTirePressure, currentTireAge);
+                    tires[tiresCounter] = tire;
+
+                    tiresCounter++;
+                }
+
+                Engine engine = new Engine(engineSpeed, enginePower);
+                Cargo cargo = new Cargo(type, cargoWeight);
+                Car car = new Car(carModel, engine, cargo, tires);
+
+                cars.Add(car);
+            }
+
+            string cargoType = Console.ReadLine();
+
+            if (cargoType == "fragile")
+            {
+                cars.Where(x => x.Cargo.Type == "fragile" && x.Tires.Any(p => p.Pressure < 1.0)).ToList().ForEach(x => Console.WriteLine(x.Model));
+            }
+
+
+            else if (cargoType == "flamable")
+            {
+                cars.Where(x => x.Cargo.Type == "flamable" && x.Engine.Power > 250).ToList().ForEach(x => Console.WriteLine(x.Model));
+            }
+        }
+    }
+}
---
+++ Advanced module/06.Defining Classes/Homework/07.RawData/Tire.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.RawData
+{
+    public class Tire
+    {
+        private double pressure;
+        private int age;
+
+        public Tire(double pressure, int age)
+        {
+            this.Age = age;
+            this.Pressure = pressure;
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                this.age = value;
+            }
+        }
+
+        public double Pressure
+        {
+            get
+            {
+                return this.pressure;
+            }
+            set
+            {
+                this.pressure = value;
+            }
+        }
+    }
+}

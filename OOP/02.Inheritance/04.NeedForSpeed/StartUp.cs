66b3aa64c669fee8982d9c8028cb9df8df8002e4
Milen Dinev <dinev.milen@gmail.com>
2020-07-22 14:04:29 +0300

    Initial Commit

---
+++ OOP/02.Inheritance/04.NeedForSpeed/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace NeedForSpeed
+{
+    using System;
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Vehicle vehicle = new Vehicle(200, 50);
+            Car car = new Car(200, 50);
+            SportCar sportCar = new SportCar(200, 50);
+            Motorcycle motorCycle = new Motorcycle(200, 50);
+
+            Console.WriteLine(vehicle);
+            Console.WriteLine(motorCycle);
+            Console.WriteLine(car);
+            Console.WriteLine(sportCar);
+
+            vehicle.Drive(20);
+            car.Drive(20);
+            sportCar.Drive(20);
+            motorCycle.Drive(20);
+
+            Console.WriteLine(vehicle);
+            Console.WriteLine(motorCycle);
+            Console.WriteLine(car);
+            Console.WriteLine(sportCar);
+        }
+    }
+}

cc8d93f9441872749320fb785874bd725a8670d0
Milen Dinev <dinev.milen@gmail.com>
2020-09-25 18:16:59 +0300

    Initial Commit

---
+++ Advanced module/ExamTasks/ClubParty/StartUp.cs
@@ -0,0 +1,57 @@
+﻿namespace ClubParty
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            int hallsCapacity = int.Parse(Console.ReadLine());
+            Dictionary<string, List<int>> reservations = new Dictionary<string, List<int>>();
+            Stack<string> input = new Stack<string>(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries));
+            Queue<string> halls = new Queue<string>();
+
+            while (input.Any())
+            {
+                string currentElement = input.Peek();
+
+                if (!char.IsDigit(currentElement[0]))
+                {
+                    reservations[currentElement] = new List<int>();
+                    halls.Enqueue(currentElement);
+                    input.Pop();
+                    continue;
+                }
+
+                if (!reservations.Any())
+                {
+                    input.Pop();
+                    continue;
+                }
+
+                foreach (var hall in reservations)
+                {
+                    int currentReservation = int.Parse(currentElement);
+
+                    if (hall.Value.Sum() + currentReservation  <= hallsCapacity)
+                    {
+                        reservations[hall.Key].Add(currentReservation);
+                        input.Pop();
+                        break;
+                    }
+
+                    if (hall.Value.Sum() + currentReservation >= hallsCapacity && halls.Any())
+                    {
+                        Console.WriteLine($"{halls.Dequeue()} -> {string.Join(", ", hall.Value)}");
+                        reservations.Remove(hall.Key);
+                    }
+
+                    break;
+
+                }
+            }
+        }
+    }
+}

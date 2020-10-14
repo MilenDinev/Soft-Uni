73623d49fa29ce652e524bffda6202adf15866e6
Milen Dinev <dinev.milen@gmail.com>
2020-10-14 15:20:44 +0300

    Initial Commit

---
+++ Advanced module/ExamTasks/Bee/StartUp.cs
@@ -0,0 +1,250 @@
+﻿namespace Bee
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int cols = rows;
+            char[,] field = new char[rows, cols];
+            int beeRow = 0;
+            int beeCol = 0;
+            int pollinatedFlowers = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                string input = Console.ReadLine();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    field[row, col] = input[col];
+
+                    if (field[row, col] == 'B')
+                    {
+                        beeRow = row;
+                        beeCol = col;
+                    }
+                }
+            }
+
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                switch (command)
+                {
+                    case "right":
+
+                        if (beeCol + 1 < cols)
+                        {
+                            field[beeRow, beeCol] = '.';
+
+                            if (field[beeRow, beeCol + 1] == 'f')
+                            {
+                                pollinatedFlowers++;
+                            }
+                            else if (field[beeRow, beeCol + 1] == 'O')
+                            {
+                                field[beeRow, beeCol + 1] = 'B';
+                                beeCol++;
+
+                                if (beeCol + 1 < cols)
+                                {
+                                    field[beeRow, beeCol] = '.';
+
+                                    if (field[beeRow, beeCol + 1] == 'f')
+                                    {
+                                        pollinatedFlowers++;
+                                    }
+                                }
+                                else
+                                {
+                                    field[beeRow, beeCol] = '.';
+                                    beeCol++;
+                                    Console.WriteLine("The bee got lost!");
+                                    break;
+                                }
+                            }
+
+                            field[beeRow, beeCol + 1] = 'B';
+                            beeCol++;
+                        }
+                        else
+                        {
+                            field[beeRow, beeCol] = '.';
+                            beeCol++;
+                            Console.WriteLine("The bee got lost!");
+                            break;
+                        }
+
+                        break;
+                    case "left":
+
+                        if (beeCol - 1 >= 0)
+                        {
+                            field[beeRow, beeCol] = '.';
+
+                            if (field[beeRow, beeCol - 1] == 'f')
+                            {
+                                pollinatedFlowers++;
+                            }
+                            else if (field[beeRow, beeCol - 1] == 'O')
+                            {
+                                field[beeRow, beeCol - 1] = 'B';
+                                beeCol--;
+
+                                if (beeCol - 1 >= 0)
+                                {
+                                    field[beeRow, beeCol] = '.';
+
+                                    if (field[beeRow, beeCol - 1] == 'f')
+                                    {
+                                        pollinatedFlowers++;
+                                    }
+                                }
+                                else
+                                {
+                                    field[beeRow, beeCol] = '.';
+                                    beeCol--;
+                                    Console.WriteLine("The bee got lost!");
+                                    break;
+                                }
+                            }
+
+                            field[beeRow, beeCol - 1] = 'B';
+                            beeCol--;
+                        }
+                        else
+                        {
+                            field[beeRow, beeCol] = '.';
+                            beeCol--;
+                            Console.WriteLine("The bee got lost!");
+                            break;
+                        }
+
+                        break;
+                    case "up":
+                        if (beeRow - 1 >= 0)
+                        {
+                            field[beeRow, beeCol] = '.';
+
+                            if (field[beeRow - 1, beeCol] == 'f')
+                            {
+                                pollinatedFlowers++;
+                            }
+                            else if (field[beeRow - 1, beeCol] == 'O')
+                            {
+                                field[beeRow - 1, beeCol] = 'B';
+                                beeRow--;
+
+                                if (beeRow - 1 >= 0)
+                                {
+                                    field[beeRow, beeCol] = '.';
+
+                                    if (field[beeRow - 1, beeCol] == 'f')
+                                    {
+                                        pollinatedFlowers++;
+                                    }
+                                }
+                                else
+                                {
+                                    field[beeRow, beeCol] = '.';
+                                    beeRow--;
+                                    Console.WriteLine("The bee got lost!");
+                                    break;
+                                }
+                            }
+
+                            field[beeRow - 1, beeCol] = 'B';
+                            beeRow--;
+                        }
+                        else
+                        {
+                            field[beeRow, beeCol] = '.';
+                            beeRow--;
+                            Console.WriteLine("The bee got lost!");
+                            break;
+                        }
+
+                        break;
+                    case "down":
+                        if (beeRow + 1 < rows)
+                        {
+                            field[beeRow, beeCol] = '.';
+
+                            if (field[beeRow + 1, beeCol] == 'f')
+                            {
+                                pollinatedFlowers++;
+                            }
+                            else if (field[beeRow + 1, beeCol] == 'O')
+                            {
+                                field[beeRow + 1, beeCol] = 'B';
+                                beeRow++;
+
+                                if (beeRow + 1 < rows)
+                                {
+                                    field[beeRow, beeCol] = '.';
+
+                                    if (field[beeRow + 1, beeCol] == 'f')
+                                    {
+                                        pollinatedFlowers++;
+                                    }
+                                }
+                                else
+                                {
+                                    field[beeRow, beeCol] = '.';
+                                    beeRow++;
+                                    Console.WriteLine("The bee got lost!");
+                                    break;
+                                }
+                            }
+
+                            field[beeRow + 1, beeCol] = 'B';
+                            beeRow++;
+                        }
+                        else
+                        {
+                            field[beeRow, beeCol] = '.';
+                            beeRow++;
+                            Console.WriteLine("The bee got lost!");
+                            break;
+                        }
+
+                        break;
+                    default:
+                        break;
+                }
+
+                if (beeCol < cols && beeCol >= 0 && beeRow < rows && beeRow >= 0)
+                {
+                    command = Console.ReadLine();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (pollinatedFlowers >= 5)
+            {
+                Console.WriteLine($"Great job, the bee managed to pollinate {pollinatedFlowers} flowers!");
+            }
+            else
+            {
+                Console.WriteLine($"The bee couldn't pollinate the flowers, she needed {5 - pollinatedFlowers} flowers more");
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write(field[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Advanced module/ExamTasks/SantasPresentFactory/StartUp.cs
@@ -0,0 +1,131 @@
+﻿namespace SantasPresentFactory
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Security.Cryptography.X509Certificates;
+
+    class StartUp
+    {
+        static void Main()
+        {
+            Stack<int> materials = new Stack<int>(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Queue<int> magicLevelValues = new Queue<int>(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+            Dictionary<string, int> toys = new Dictionary<string, int>()
+            {
+             {"Doll", 0},
+             {"Wooden train", 0},
+             {"Teddy bear", 0},
+             {"Bicycle", 0}
+            };
+
+
+            while (materials.Any() && magicLevelValues.Any())
+            {
+                int currentMaterial = materials.Peek();
+                int currentMagicLevel = magicLevelValues.Peek();
+
+                if (currentMaterial == 0 || currentMagicLevel == 0)
+                {
+                    if (currentMaterial == 0)
+                    {
+                        materials.Pop();
+                    }
+                    if (currentMagicLevel == 0)
+                    {
+                        magicLevelValues.Dequeue();
+                    }
+                    continue;
+                }
+
+
+                int sum = currentMaterial * currentMagicLevel;
+
+                if (sum < 0)
+                {
+                    int currentSum = currentMaterial + currentMagicLevel;
+                    materials.Pop();
+                    magicLevelValues.Dequeue();
+                    materials.Push(currentSum);
+                    continue;
+                }
+
+                if ((sum != 150 && sum !=250 && sum != 300 && sum != 400) && sum > 0)
+                {
+                    magicLevelValues.Dequeue();
+                    int increaseMats = materials.Pop() + 15;
+                    materials.Push(increaseMats);
+
+                    continue;
+                }
+
+
+                if (sum == 150)
+                {
+                    toys["Doll"]++;
+
+                    materials.Pop();
+                    magicLevelValues.Dequeue();
+                    continue;
+                }
+
+                if (sum == 250)
+                {
+                    toys["Wooden train"]++;
+                    materials.Pop();
+                    magicLevelValues.Dequeue();
+                    continue;
+                }
+
+                if (sum == 300)
+                {
+                    toys["Teddy bear"]++;
+                    materials.Pop();
+                    magicLevelValues.Dequeue();
+                    continue;
+                }
+
+                if (sum == 400)
+                {
+                    toys["Bicycle"]++;
+                    materials.Pop();
+                    magicLevelValues.Dequeue();
+                    continue;
+                }
+
+            }
+
+
+            bool firstCouple = toys["Doll"] > 0 && toys["Wooden train"] > 0;
+            bool secondCouple = toys["Teddy bear"] > 0 && toys["Bicycle"] > 0;
+
+            if (firstCouple || secondCouple)
+            {
+                Console.WriteLine("The presents are crafted! Merry Christmas!");
+
+
+            }
+
+            else if (!firstCouple && !secondCouple)
+            {
+                Console.WriteLine("No presents this Christmas!");
+            }
+
+            if (materials.Any())
+            {
+                Console.WriteLine($"Materials left: {string.Join(", ", materials)}");
+            }
+
+            if (magicLevelValues.Any())
+            {
+                Console.WriteLine($"Magic left: {string.Join(", ", magicLevelValues)}");
+            }
+
+            foreach (var toy in toys.OrderBy(x=> x.Key).Where(x => x.Value > 0))
+            {
+                Console.WriteLine($"{toy.Key}: {toy.Value}");
+            }
+        }
+    }
+}

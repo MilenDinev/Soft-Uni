02787441ce054bd9e71001e1df546ca560876d97
Milen Dinev <dinev.milen@gmail.com>
2021-01-29 17:47:35 +0200

    New Solution Added

--- Databases module/MS SQL Server/06. INDICES DATA AGGREGATION/Homework/Homework.sql
+++ Databases module/MS SQL Server/06. INDICES DATA AGGREGATION/Homework/Homework.sql
@@ -93,3 +93,18 @@ GROUP BY DepartmentID
 HAVING DepartmentID IN (2,5,7)
 
 --15
+SELECT * 
+INTO NewTable
+FROM Employees
+WHERE Salary > 30000
+
+DELETE FROM NewTable
+WHERE ManagerID = 42
+
+UPDATE NewTable
+SET Salary+=5000
+WHERE DepartmentID = 1
+
+SELECT DepartmentID, AVG(Salary) AS AverageSalary
+FROM NewTable
+GROUP BY DepartmentID

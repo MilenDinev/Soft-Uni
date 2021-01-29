ab32070e3afce55c9b78e802772f593fa6386265
Milen Dinev <dinev.milen@gmail.com>
2021-01-29 03:03:32 +0200

    New tasks added

--- Databases module/MS SQL Server/06. INDICES DATA AGGREGATION/Homework/Homework.sql
+++ Databases module/MS SQL Server/06. INDICES DATA AGGREGATION/Homework/Homework.sql
@@ -1,8 +1,64 @@
-SELECT DepartmentID, SUM(Salary) As Salary 
-FROM Employees AS e
-GROUP BY DepartmentID  
-ORDER BY Salary
+--SELECT DepartmentID, SUM(Salary) As Salary 
+--FROM Employees AS e
+--GROUP BY DepartmentID  
+--ORDER BY Salary
 
- SELECT DepartmentID, MIN(Salary) AS MinSalary
- FROM Employees
- GROUP BY DepartmentID
+-- SELECT DepartmentID, MIN(Salary) AS MinSalary
+-- FROM Employees
+-- GROUP BY DepartmentID
+
+ SELECT *
+ FROM WizzardDeposits
+
+--1
+ SELECT COUNT(*) AS [Count]
+ FROM WizzardDeposits
+
+ --2
+
+ SELECT MAX(MagicWandSize) AS LongestMagicWand
+ FROM WizzardDeposits
+
+ --3
+ SELECT DepositGroup, MAX(MagicWandSize) AS LongestMagicWand
+ FROM WizzardDeposits
+ GROUP BY DepositGroup
+
+ --4
+ SELECT DepositGroup, MAX(MagicWandSize) AS LongestMagicWand
+ FROM WizzardDeposits
+ GROUP BY DepositGroup
+
+ --5
+ SELECT DepositGroup, SUM(DepositAmount) AS TotalSum
+ FROM WizzardDeposits
+ GROUP BY DepositGroup
+
+ --6
+ SELECT DepositGroup, SUM(DepositAmount) AS TotalSum
+ FROM WizzardDeposits
+ GROUP BY DepositGroup, MagicWandCreator
+ HAVING(MagicWandCreator) = 'Ollivander family'
+
+ --7
+ SELECT DepositGroup, SUM(DepositAmount) AS TotalSum
+ FROM WizzardDeposits
+ WHERE MagicWandCreator = 'Ollivander family'
+ GROUP BY DepositGroup
+ HAVING SUM(DepositAmount) < 150000
+ ORDER BY TotalSum DESC
+
+ --8
+
+ SELECT DepositGroup, MagicWandCreator, MIN(DepositCharge)
+ FROM WizzardDeposits
+ GROUP BY DepositGroup, MagicWandCreator
+ ORDER BY MagicWandCreator, DepositGroup
+
+ --9
+
+ --10
+SELECT DISTINCT LEFT(FirstName, 1) AS FirstLetter
+FROM WizzardDeposits
+WHERE DepositGroup = 'Troll Chest'
+ORDER BY FirstLetter

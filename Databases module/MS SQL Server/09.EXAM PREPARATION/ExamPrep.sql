6e3035335f23e7a627a6a49b2f0bdc8132c017d5
Milen Dinev <dinev.milen@gmail.com>
2021-02-10 23:03:42 +0200

    New tasks added

--- Databases module/MS SQL Server/09.EXAM PREPARATION/ExamPrep.sql
+++ Databases module/MS SQL Server/09.EXAM PREPARATION/ExamPrep.sql
@@ -172,9 +172,91 @@ LEFT JOIN Orders AS o ON j.JobId = o.JobId
 WHERE  j.[Status] <> 'Finished' AND p.StockQty + IIF(o.Delivered = 0, op.Quantity, 0) < pn.Quantity
 ORDER BY p.PartId ASC
 
-SELECT *
 
-FROM Parts p
-LEFT JOIN PartsNeeded AS pn ON p.PartId = pn.PartId
-LEFT JOIN OrderParts AS op ON pn.PartId = op.PartId 
-WHERE (p.StockQty + op.Quantity) - pn.Quantity < 0
+CREATE OR ALTER PROCEDURE usp_PlaceOrder
+( 
+	@jobId INT,
+	@serialNumber VARCHAR(50),
+	@qty INT
+)
+AS
+
+ DECLARE @status VARCHAR(10) = (SELECT [Status] FROM Jobs WHERE JobId = @jobId)
+
+ DECLARE @partId VARCHAR(10) = (SELECT PartId FROM Parts WHERE SerialNumber = @serialNumber)
+
+IF (@qty <= 0)
+		THROW 50012, 'Part quantity must be more than zero!', 1
+ELSE IF (@status = 'Finished')
+	THROW 50011, 'This job is not active!', 1
+ELSE IF (@status IS NULL)
+		THROW 50013, 'Job not found', 1
+ELSE IF (@partId IS NULL)
+		THROW 50014, 'Part not found', 1
+
+DECLARE @orderId INT = (SELECT o.OrderId 
+						FROM Orders AS o
+						JOIN OrderParts AS op ON op.OrderId = o.OrderId
+						JOIN Parts AS p ON p.PartId = op.PartId
+						WHERE JobId = @jobId AND p.PartId = @partId)
+
+IF (@orderId IS NULL)
+ BEGIN
+	INSERT INTO Orders (JobId, IssueDate) VALUES
+	(@jobId, NULL)
+
+	SET @orderId = (SELECT OrderId FROM Orders WHERE JobId = @jobId AND IssueDate IS NULL)
+
+	INSERT INTO OrderParts (OrderId, PartId, Quantity) VALUES
+	(@orderId, @partId, @qty)
+
+ END
+ELSE 
+ BEGIN
+	DECLARE @issueDate DATE = (SELECT IssueDate FROM Orders WHERE OrderId = @orderId AND JobId = @jobId)
+
+	IF (@issueDate IS NULL)
+	INSERT INTO OrderParts (OrderId, PartId, Quantity) VALUES
+	(@orderId, @partId, @qty)
+	ELSE 
+		UPDATE OrderParts
+		SET Quantity += @qty
+		WHERE OrderId =  @orderId AND PartId = @partId
+ END
+
+
+DECLARE @err_msg AS NVARCHAR(MAX);
+BEGIN TRY
+  EXEC usp_PlaceOrder 1, 'ZeroQuantity', 0
+END TRY
+
+BEGIN CATCH
+  SET @err_msg = ERROR_MESSAGE();
+  SELECT @err_msg
+END CATCH
+
+
+GO
+
+--12 
+
+
+CREATE FUNCTION udf_GetCost (@jobId INT)
+RETURNS DECIMAL(15,2)
+AS
+BEGIN
+DECLARE @result DECIMAL (15,2)
+SET @result = (SELECT SUM(p.Price * op.Quantity	) AS totalSym
+	FROM Jobs AS j
+	JOIN Orders AS o ON o.JobId = j.JobId
+	JOIN OrderParts AS op ON op.OrderId = o.OrderId
+	JOIN Parts AS p ON p.PartId = op.PartId
+	WHERE j.JobId = @jobId
+	GROUP BY j.JobId) 
+
+IF (@result IS NULL)
+	SET @result = 0;
+
+RETURN @result
+
+END

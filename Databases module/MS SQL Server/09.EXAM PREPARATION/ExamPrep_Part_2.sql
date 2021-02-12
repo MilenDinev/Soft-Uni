c94ee5f3f8e261ded9014155d6a587f777a4623d
Milen Dinev <dinev.milen@gmail.com>
2021-02-12 14:48:13 +0200

    New tasks added

--- Databases module/MS SQL Server/09.EXAM PREPARATION/ExamPrep_Part_2.sql
+++ Databases module/MS SQL Server/09.EXAM PREPARATION/ExamPrep_Part_2.sql
@@ -100,3 +100,15 @@ FROM Cities AS c
 JOIN Hotels AS h ON c.Id = h.CityId
 GROUP BY c.[Name]
 ORDER BY COUNT(h.Id) DESC, c.[Name]
+
+--7
+
+SELECT a.Id, CONCAT(a.FirstName,' ', a.LastName) AS FullName, 
+			MAX(DATEDIFF(DAY,t.ArrivalDate, t.ReturnDate)) AS LongestTrip, 
+			MIN(DATEDIFF(DAY,t.ArrivalDate, t.ReturnDate)) AS ShortestTrip
+FROM Accounts AS a
+JOIN AccountsTrips AS [at] ON a.Id = [at].AccountId
+JOIN Trips AS t ON [at].TripId = t.Id
+WHERE a.MiddleName IS NULL AND t.CancelDate IS NULL
+GROUP BY a.Id, CONCAT(a.FirstName,' ', a.LastName)
+ORDER BY LongestTrip DESC, ShortestTrip ASC

6127679a61edd4f4b47721dd320170eb70859ed6
Milen Dinev <dinev.milen@gmail.com>
2021-02-13 12:30:39 +0200

    New tasks added

--- Databases module/MS SQL Server/10.EXAM/Exam.sql
+++ Databases module/MS SQL Server/10.EXAM/Exam.sql
@@ -62,3 +62,11 @@ VALUES
 ('Find.java', 9957.86, 5, 5),
 ('Controller.json', 14034.87, 3, 6),
 ('Operate.xix', 7662.92, 7,	7)
+
+
+INSERT INTO Issues (Title, IssueStatus, RepositoryId, AssigneeId)
+VALUES
+('Critical Problem with HomeController.cs file', 'open', 1, 4),
+('Typo fix in Judge.html', 'open', 4, 3),
+('Implement documentation for UsersService.cs', 'closed', 8, 2),
+('Unreachable code in Index.cs', 'open', 9, 8)

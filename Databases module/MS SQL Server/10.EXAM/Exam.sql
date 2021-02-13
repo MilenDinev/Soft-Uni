a35507bf8996e3469085b15fca3ae158bdfbd2c0
Milen Dinev <dinev.milen@gmail.com>
2021-02-13 12:30:17 +0200

    New tasks added

--- Databases module/MS SQL Server/10.EXAM/Exam.sql
+++ Databases module/MS SQL Server/10.EXAM/Exam.sql
@@ -50,3 +50,15 @@ CREATE TABLE Files
 	ParentId INT FOREIGN KEY REFERENCES Files(Id),
 	CommitId INT NOT NULL FOREIGN KEY REFERENCES Commits(Id)
 )
+
+
+--2
+INSERT INTO Files ([Name], Size, ParentId, CommitId)
+VALUES
+('Trade.idk', 2598.0, 1, 1),
+('menu.net', 9238.31, 2, 2),
+('Administrate.soshy', 1246.93,	3, 3),
+('Controller.php', 7353.15,	4, 4),
+('Find.java', 9957.86, 5, 5),
+('Controller.json', 14034.87, 3, 6),
+('Operate.xix', 7662.92, 7,	7)

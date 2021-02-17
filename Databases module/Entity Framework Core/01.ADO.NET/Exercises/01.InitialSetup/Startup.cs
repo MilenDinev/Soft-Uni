9f5d1e9e7b665ca7b2025b75789ed32fe4e5d571
Milen Dinev <dinev.milen@gmail.com>
2021-02-17 18:12:20 +0200

    New tasks added

--- Databases module/Entity Framework Core/01.ADO.NET/Exercises/01.InitialSetup/Startup.cs
+++ Databases module/Entity Framework Core/01.ADO.NET/Exercises/01.InitialSetup/Startup.cs
@@ -36,13 +36,35 @@
                 command.Parameters.AddWithValue("@Id", id);
                 var result = command.ExecuteScalar();
 
+                string minnionsQuery = @"SELECT ROW_NUMBER() OVER (ORDER BY m.Name) AS RowNum, m.Name, m.Age 
+                                            FROM MinionsVillains AS mv 
+                                            JOIN Minions AS m ON mv.MinionId = m.Id
+                                            WHERE mv.VillainId = @Id
+                                        ORDER BY m.Name";
+
                 if (result == null)
                 {
                     Console.WriteLine($"No villain with ID {id} exists in the database.");
                 }
                 else
                 {
-                    Console.WriteLine(result);
+                    Console.WriteLine($"Villain: {result}");
+                    using (var minionsCommnad = new SqlCommand(minnionsQuery, connection))
+                    {
+                        minionsCommnad.Parameters.AddWithValue("@Id", id);
+                        using (var reader = minionsCommnad.ExecuteReader())
+                        {
+                            if (!reader.HasRows)
+                            {
+                                Console.WriteLine("(no minions)");
+                            }
+                            while (reader.Read())
+                            {
+                                Console.WriteLine($"{reader[0]}. {reader[1]} {reader[2]}");
+                            }
+                        }
+
+                    }
                 }
 
 

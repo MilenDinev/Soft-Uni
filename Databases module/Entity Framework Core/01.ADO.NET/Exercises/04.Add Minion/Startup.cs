681546c853e5891f3ef6c64c8e7ca3fe90acdf39
Milen Dinev <dinev.milen@gmail.com>
2021-02-17 18:54:52 +0200

    New tasks added

---
+++ Databases module/Entity Framework Core/01.ADO.NET/Exercises/04.Add Minion/Startup.cs
@@ -0,0 +1,35 @@
+﻿namespace _04.Add_Minion
+{
+    using Microsoft.Data.SqlClient;
+    using System;
+
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection("Server=.;Integrated Security=true;Database=MinionsDB");
+
+            using (connection)
+            {
+                connection.Open();
+                string[] input = Console.ReadLine().Split();
+                string minionName = input[1];
+                int age = int.Parse(input[2]);
+                string town = input[3];
+
+                if (input[3] == "Stara")
+                {
+                    town = input[3] + ' ' + input[4];
+                }
+
+                string townIdQuery = "SELECT Id FROM Towns WHERE Name = @town";
+                using SqlCommand command = new SqlCommand(townIdQuery, connection);
+                command.Parameters.AddWithValue("@town", town);
+                var result = command.ExecuteScalar();
+                Console.WriteLine(result);
+
+
+            }
+        }
+    }
+}

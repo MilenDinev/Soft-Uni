de5e8ccc2c85743a3e3f821d7001ca9675c9d7a8
Milen Dinev <dinev.milen@gmail.com>
2021-02-22 22:02:21 +0200

    Update

--- Databases module/Entity Framework Core/03.ENTITY FRAMEWORK INTRODUCTION/EFCoreIntroductionDemo/EFCoreCodeFirstDemo/Models/Comment.cs
+++ Databases module/Entity Framework Core/03.ENTITY FRAMEWORK INTRODUCTION/EFCoreIntroductionDemo/EFCoreCodeFirstDemo/Models/Comment.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace EFCoreCodeFirstDemo.Models
 {
-    class Comment
+    [Index(nameof(QuestionId), Name = "IX_QuestionIdMil")]
+    public class Comment
     {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Content { get; set; }
+
+        public int QuestionId { get; set; }
+        public Question Question { get; set; }
     }
 }

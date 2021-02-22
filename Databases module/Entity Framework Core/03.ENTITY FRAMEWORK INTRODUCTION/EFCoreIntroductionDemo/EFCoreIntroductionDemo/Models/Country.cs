f2704d761355030267348ec13a1d0665c63d1754
Milen Dinev <dinev.milen@gmail.com>
2021-02-22 18:53:36 +0200

    SoftUni Database classes created

---
+++ Databases module/Entity Framework Core/03.ENTITY FRAMEWORK INTRODUCTION/EFCoreIntroductionDemo/EFCoreIntroductionDemo/Models/Country.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EFCoreIntroductionDemo.Models
+{
+    public partial class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Databases module/Entity Framework Core/03.ENTITY FRAMEWORK INTRODUCTION/EFCoreIntroductionDemo/EFCoreIntroductionDemo/Models/VEmployeesHiredAfter2000.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EFCoreIntroductionDemo.Models
+{
+    public partial class VEmployeesHiredAfter2000
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime HireDate { get; set; }
+    }
+}

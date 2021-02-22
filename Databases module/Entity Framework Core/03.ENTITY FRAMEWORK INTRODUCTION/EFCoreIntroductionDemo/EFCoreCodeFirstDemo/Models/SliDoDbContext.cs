5692e8f941443a8a9a821ce212eef1afdbec59f8
Milen Dinev <dinev.milen@gmail.com>
2021-02-22 22:00:30 +0200

    Update

---
+++ Databases module/Entity Framework Core/03.ENTITY FRAMEWORK INTRODUCTION/EFCoreIntroductionDemo/EFCoreCodeFirstDemo/Models/SliDoDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreCodeFirstDemo.Models
+{
+  public  class SliDoDbContext : DbContext
+    {
+
+        public SliDoDbContext()
+        {
+
+        }
+
+        public SliDoDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            if (!optionBuilder.IsConfigured)
+            {
+                optionBuilder.UseSqlServer("Server=.;DatabaseSliDo;Integrated Security=true");
+            }
+        }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Question> Questions { get; set; }
+    }
+}

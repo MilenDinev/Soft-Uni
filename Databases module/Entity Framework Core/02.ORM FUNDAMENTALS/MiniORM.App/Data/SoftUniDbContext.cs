78ee261708f73bb2f4fa511a46888eac6a60ff18
Milen Dinev <dinev.milen@gmail.com>
2021-02-22 03:14:45 +0200

    New tasks added

--- Databases module/Entity Framework Core/02.ORM FUNDAMENTALS/MiniORM.App/Data/SoftUniDbContext.cs
+++ Databases module/Entity Framework Core/02.ORM FUNDAMENTALS/MiniORM.App/Data/SoftUniDbContext.cs
@@ -1,20 +1,20 @@
 ﻿namespace MiniORM.App.Data
 {
-	using Entities;
+    using Entities;
 
-	public class SoftUniDbContext : DbContext
-	{
-		public SoftUniDbContext(string connectionString)
-			: base(connectionString)
-		{
-		}
+    public class SoftUniDbContext : DbContext
+    {
+        public SoftUniDbContext(string connectionString)
+    : base(connectionString)
+        {
+        }
 
-		public DbSet<Employee> Employees { get; }
+        public DbSet<Employee> Employees { get; }
 
-		public DbSet<Department> Departments { get; }
+        public DbSet<Department> Departments { get; }
 
-		public DbSet<Project> Projects { get; }
+        public DbSet<Project> Projects { get; }
 
-		public DbSet<EmployeeProject> EmployeesProjects { get; }
-	}
+        public DbSet<EmployeeProject> EmployeesProjects { get; }
+    }
 }

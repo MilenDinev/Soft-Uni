6c59820c1c1209815bf9efed39b5d87aaa949945
Milen Dinev <dinev.milen@gmail.com>
2021-02-22 18:39:13 +0200

    MicrosoftEFCoreDesigned installed

--- Databases module/Entity Framework Core/03.ENTITY FRAMEWORK INTRODUCTION/EFCoreIntroductionDemo/EFCoreIntroductionDemo/Program.cs
+++ Databases module/Entity Framework Core/03.ENTITY FRAMEWORK INTRODUCTION/EFCoreIntroductionDemo/EFCoreIntroductionDemo/Program.cs
@@ -3,7 +3,7 @@
     using System;
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("Hello World!");
         }

973134706b8daa0322919698cd4865fe84622061
Milen Dinev <dinev.milen@gmail.com>
2021-02-23 03:49:53 +0200

    New Tasks Added

---
+++ Databases module/Entity Framework Core/03.ENTITY FRAMEWORK INTRODUCTION/Homework/03.EmployeesInformation/StartUp.cs
@@ -0,0 +1,44 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var db = new SoftUniContext();
+
+
+            System.Console.WriteLine(GetEmployeesFullInformation(db));
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            var employeesInfo = context.Employees
+                .Select(x => new { x.EmployeeId, x.FirstName, x.LastName, x.MiddleName, x.JobTitle, x.Salary })
+                .OrderBy(x => x.EmployeeId).ToList();
+
+            foreach (var employee in employeesInfo)
+            {
+                sb.Append($"{employee.FirstName} {employee.LastName}");
+                if (!string.IsNullOrWhiteSpace(employee.MiddleName))
+                {
+                    sb.Append($" {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+                }
+
+                else
+                {
+                    sb.Append($" {employee.JobTitle} {employee.Salary:f2}");
+                }
+
+                sb.AppendLine();
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ Databases module/Entity Framework Core/03.ENTITY FRAMEWORK INTRODUCTION/Homework/04..EmployeesWithSalaryOver50000/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var db = new SoftUniContext();
+
+            System.Console.WriteLine(GetEmployeesWithSalaryOver50000(db));
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            var employeesSalaryInfo = context.Employees
+                .Select(x => new {x.FirstName,x.Salary })
+                .Where(x => x.Salary > 50_000)
+                .OrderBy(x => x.FirstName).ToList();
+
+            foreach (var employee in employeesSalaryInfo)
+            {
+                sb.AppendLine($"{employee.FirstName} - {employee.Salary:f2}");
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}

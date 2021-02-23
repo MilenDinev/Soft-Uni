f6f0986b62ada0a230fbc94161651e9a4683117f
Milen Dinev <dinev.milen@gmail.com>
2021-02-23 03:25:32 +0200

    logic changed

--- Databases module/Entity Framework Core/03.ENTITY FRAMEWORK INTRODUCTION/Homework/02.DatabaseFirst/StartUp.cs
+++ Databases module/Entity Framework Core/03.ENTITY FRAMEWORK INTRODUCTION/Homework/02.DatabaseFirst/StartUp.cs
@@ -2,27 +2,43 @@
 {
     using SoftUni.Data;
     using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
 
     public class StartUp
     {
         static void Main(string[] args)
         {
             var db = new SoftUniContext();
-            GetEmployeesFullInformation(db);
+
+
+            System.Console.WriteLine(GetEmployeesFullInformation(db));
         }
 
         public static string GetEmployeesFullInformation(SoftUniContext context)
         {
+            StringBuilder sb = new StringBuilder();
             var employeesInfo = context.Employees
-                .Select(x => new { x.EmployeeId, x.FirstName, x.LastName, x.MiddleName, x.JobTitle, x.Salary})
+                .Select(x => new { x.EmployeeId, x.FirstName, x.LastName, x.MiddleName, x.JobTitle, x.Salary })
                 .OrderBy(x => x.EmployeeId).ToList();
 
             foreach (var employee in employeesInfo)
             {
-                System.Console.WriteLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2} ");
+                sb.Append($"{employee.FirstName} {employee.LastName}");
+                if (!string.IsNullOrWhiteSpace(employee.MiddleName))
+                {
+                    sb.Append($" {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+                }
+
+                else
+                {
+                    sb.Append($" {employee.JobTitle} {employee.Salary:f2}");
+                }
+
+                sb.AppendLine();
             }
 
-            return null;
+            return sb.ToString().Trim();
         }
     }
 }

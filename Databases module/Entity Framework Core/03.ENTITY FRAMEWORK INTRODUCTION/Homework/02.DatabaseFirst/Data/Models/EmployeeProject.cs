3b2933cf93ff7c2b2a9c03d7480781a7c8fa9c93
Milen Dinev <dinev.milen@gmail.com>
2021-02-23 02:08:47 +0200

    Task 02 added

---
+++ Databases module/Entity Framework Core/03.ENTITY FRAMEWORK INTRODUCTION/Homework/02.DatabaseFirst/Data/Models/EmployeeProject.cs
@@ -0,0 +1,11 @@
+﻿namespace _02.DatabaseFirst.Data.Models
+{
+    public partial class EmployeeProject
+    {
+        public int EmployeeId { get; set; }
+        public int ProjectId { get; set; }
+
+        public virtual Employee Employee { get; set; }
+        public virtual Project Project { get; set; }
+    }
+}
---
+++ Databases module/Entity Framework Core/03.ENTITY FRAMEWORK INTRODUCTION/Homework/02.DatabaseFirst/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace _02.DatabaseFirst
+{
+    using Data.Models;
+    using System.Linq;
+    using System.Security.Cryptography.X509Certificates;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var db = new SoftUniContext();
+            GetEmployeesFullInformation(db);
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employeesInfo = context.Employees
+                .Select(x => new { x.EmployeeId, x.FirstName, x.LastName, x.MiddleName, x.JobTitle, x.Salary})
+                .OrderBy(x => x.EmployeeId).ToList();
+
+            foreach (var employee in employeesInfo)
+            {
+                System.Console.WriteLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.Salary:f2} ");
+            }
+
+            return "";
+        }
+    }
+}

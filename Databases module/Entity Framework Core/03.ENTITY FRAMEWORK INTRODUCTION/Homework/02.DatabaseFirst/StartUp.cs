47804ec0c14a611357b92649ce9b7f6d46e0aa41
Milen Dinev <dinev.milen@gmail.com>
2021-02-24 14:54:46 +0200

    Task 6 added

--- Databases module/Entity Framework Core/03.ENTITY FRAMEWORK INTRODUCTION/Homework/02.DatabaseFirst/StartUp.cs
+++ Databases module/Entity Framework Core/03.ENTITY FRAMEWORK INTRODUCTION/Homework/02.DatabaseFirst/StartUp.cs
@@ -3,7 +3,6 @@
     using SoftUni.Data;
     using System.Linq;
     using System.Text;
-    using System.Text.RegularExpressions;
 
     public class StartUp
     {
@@ -11,21 +10,35 @@
         {
             var db = new SoftUniContext();
 
-            System.Console.WriteLine(GetEmployeesFromResearchAndDevelopment(db));
+            System.Console.WriteLine(GetDepartmentsWithMoreThan5Employees(db));
         }
 
-        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        public static string GetDepartmentsWithMoreThan5Employees(SoftUniContext context)
         {
             StringBuilder sb = new StringBuilder();
-            var employeesSalaryInfo = context.Employees
-                .Select(x => new {x.FirstName, x.LastName, x.Department, x.Salary })
-                .Where(x => x.Department.Name == "Research and Development")
-                .OrderBy(x => x.Salary).ThenByDescending(x => x.FirstName).ToList();
+            var departments = context.Departments
+                .Where(x => x.Employees.Count > 5)
+                .OrderBy(x => x.Employees.Count)
+                .ThenBy(x => x.Name)
+                .Select(x => new 
+                { 
+                    DepartmentName = x.Name,
+                    ManagerFirstName = x.Manager.FirstName,
+                    ManagerLastName = x.Manager.LastName,
+                    Emplooyees = x.Employees.Select( e => new { e.FirstName, e.LastName, e.JobTitle })
+                })
+                .ToList();
 
-            foreach (var employee in employeesSalaryInfo)
+            foreach (var department in departments)
             {
-                sb.AppendLine($"{employee.FirstName} {employee.LastName} from {employee.Department.Name} - ${employee.Salary:f2}");
+                sb.AppendLine($"{department.DepartmentName} - {department.ManagerFirstName} {department.ManagerLastName}");
+
+                foreach (var employee in department.Emplooyees.OrderBy(e => e.FirstName).ThenBy(e => e.LastName))
+                {
+                    sb.AppendLine($"{employee.FirstName} {employee.LastName} - {employee.JobTitle}");
+                }
             }
+
             return sb.ToString().TrimEnd();
         }
     }
---
+++ Databases module/Entity Framework Core/03.ENTITY FRAMEWORK INTRODUCTION/Homework/06.AddingNewAddressAndUpdatingEmployee/StartUp.cs
@@ -0,0 +1,45 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    using SoftUni.Models;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var db = new SoftUniContext();
+
+            System.Console.WriteLine(AddNewAddressToEmployee(db));
+        }
+
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var nakov = context.Employees
+                .FirstOrDefault(x => x.LastName == "Nakov");
+
+            nakov.Address = new Address
+            {
+                AddressText = "Vitoshka 15",
+                TownId = 4,
+            };
+
+            context.SaveChanges();
+
+            var employees = context.Employees
+                .Select(x => new { x.Address.AddressText, x.Address.AddressId })
+                .OrderByDescending(x => x.AddressId)
+                .Take(10)
+                .ToList();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.AddressText}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}

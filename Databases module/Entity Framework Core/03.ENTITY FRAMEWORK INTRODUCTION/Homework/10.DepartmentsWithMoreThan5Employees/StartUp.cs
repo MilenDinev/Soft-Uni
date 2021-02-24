6b8d40fd79ecab9e0ff6703bd9b918ff5aa287c7
Milen Dinev <dinev.milen@gmail.com>
2021-02-24 14:59:35 +0200

    Task 9 added

---
+++ Databases module/Entity Framework Core/03.ENTITY FRAMEWORK INTRODUCTION/Homework/10.DepartmentsWithMoreThan5Employees/StartUp.cs
@@ -0,0 +1,45 @@
+﻿namespace SoftUni
+{
+    using SoftUni.Data;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var db = new SoftUniContext();
+
+            System.Console.WriteLine(GetDepartmentsWithMoreThan5Employees(db));
+        }
+
+        public static string GetDepartmentsWithMoreThan5Employees(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+            var departments = context.Departments
+                .Where(x => x.Employees.Count > 5)
+                .OrderBy(x => x.Employees.Count)
+                .ThenBy(x => x.Name)
+                .Select(x => new 
+                { 
+                    DepartmentName = x.Name,
+                    ManagerFirstName = x.Manager.FirstName,
+                    ManagerLastName = x.Manager.LastName,
+                    Emplooyees = x.Employees.Select( e => new { e.FirstName, e.LastName, e.JobTitle })
+                })
+                .ToList();
+
+            foreach (var department in departments)
+            {
+                sb.AppendLine($"{department.DepartmentName} - {department.ManagerFirstName} {department.ManagerLastName}");
+
+                foreach (var employee in department.Emplooyees.OrderBy(e => e.FirstName).ThenBy(e => e.LastName))
+                {
+                    sb.AppendLine($"{employee.FirstName} {employee.LastName} - {employee.JobTitle}");
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}

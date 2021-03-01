1c2cef2996a13d0e311f672585ec6a807a4dfb63
Milen Dinev <dinev.milen@gmail.com>
2021-03-01 02:41:04 +0200

    new solutions added

---
+++ Databases module/Entity Framework Core/04.ENTITY RELATIONS/Exercises/01.StudentSystem/Data/StudentSystemContext.cs
@@ -0,0 +1,47 @@
+﻿namespace P01_StudentSystem.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using P01_StudentSystem.Data.Models;
+
+    public class StudentSystemContext : DbContext
+    {
+        //create constructor
+        // dbsets
+        // connect sql server
+        // fluent api relation
+
+        public StudentSystemContext()
+        {
+
+        }
+
+        public StudentSystemContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Homework> HomeworkSubmissions { get; set; }
+        public DbSet<Resource> Resources { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=StudentSystem;Integrated Security=True;");
+
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentCourse>(x =>
+            {
+                x.HasKey(x => new { x.CourseId, x.StudentId });
+            });
+        }
+    }
+}

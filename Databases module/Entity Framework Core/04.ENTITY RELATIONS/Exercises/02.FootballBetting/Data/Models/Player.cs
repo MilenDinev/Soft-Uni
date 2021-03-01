13afa6c653021b72572c85bf7d083a19ef5945a4
Milen Dinev <dinev.milen@gmail.com>
2021-03-01 17:40:12 +0200

    new solutions added

--- Databases module/Entity Framework Core/04.ENTITY RELATIONS/Exercises/02.FootballBetting/Data/Models/Player.cs
+++ Databases module/Entity Framework Core/04.ENTITY RELATIONS/Exercises/02.FootballBetting/Data/Models/Player.cs
@@ -1,8 +1,16 @@
 ﻿namespace P03_FootballBetting.Data.Models
 {
     using System.ComponentModel.DataAnnotations;
+
     public class Player
     {
+        public int PlayerId { get; set; }
 
+        [Required]
+        public string Name { get; set; }
+        public int SquadNumber { get; set; }
+        public int TeamId { get; set; }
+        public int PositionId { get; set; }
+        public bool IsInjured { get; set; }
     }
 }

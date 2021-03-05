0a80dcfe5e215519dca442651cac5a1538b4590b
Milen Dinev <dinev.milen@gmail.com>
2021-03-05 23:39:06 +0200

    Initial Commit

---
+++ Databases module/Entity Framework Core/04.ENTITY RELATIONS/Exercises/Test/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int firstNum = 0;
+            int secondNum = 1;
+
+            for (int i = n; i > 0; i--)
+            {
+                Console.Write($"Index: {i}, SUM: ");
+                int tempSum = firstNum + secondNum;
+                firstNum = secondNum;
+                secondNum = tempSum;
+
+                Console.Write($"{firstNum}");
+
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(firstNum);
+        }
+    }
+}
+
+//int n = int.Parse(Console.ReadLine());
+//int startingNum = n;
+
+//for (int i = n - 1; i > 1; i--)
+//{
+//    Console.Write($"Index: {i}, SUM: ");
+//    startingNum *= i;
+
+//    Console.Write($"{startingNum}");
+
+
+//    Console.WriteLine();
+//}
+
+//Console.WriteLine(startingNum);

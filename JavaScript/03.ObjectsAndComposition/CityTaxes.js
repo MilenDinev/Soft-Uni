f839f28057bd4eb31f24efde6d6c3a016f83054d
Milen Dinev <dinev.milen@gmail.com>
2021-03-05 14:28:01 +0200

    Initial Commit

---
+++ JavaScript/03.ObjectsAndComposition/CityTaxes.js
@@ -0,0 +1,30 @@
+function citiRecord(name, population, treasury){
+
+    const city = {
+        name,
+        population,
+        treasury,
+        taxRate: 10,
+        collectTaxes(){
+            this.treasury+= this.population * this.taxRate;
+        },
+        applyGrowth(percent){
+            this.population += Math.floor(this.population*percent /100);
+        },
+        applyRecession(percent){
+            this.treasury -= Math.ceil(this.treasury * percent / 100);
+        }
+    };
+
+    return city
+}
+
+const result = citiRecord('Tortuga', 7000, 15000)
+
+console.log(result);
+result.collectTaxes();
+console.log(result.treasury);
+result.applyGrowth(10);
+result.applyRecession(5);
+
+console.log(result);

cf5da1580e7a809c8b5c934e98fce0c43b0ba6f1
Milen Dinev <dinev.milen@gmail.com>
2021-03-11 18:50:03 +0200

    InitialCommit

---
+++ Databases module/Entity Framework Core/08.JSON PROCESSING/JSONDemo/JSONDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+namespace JSONDemo
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car
+            {
+                Extras = new List<string> { "klimatronik", "4x4", "Farove" },
+                ManufacturedOn = DateTime.Now,
+                Model = "Golf",
+                Vendor = "VW",
+                Price = 12.323m,
+                Engine = new Engine { 
+                    HorsePower = 200, 
+                    Volume = 1.6f 
+                }
+            };
+
+            JsonSerializer.Serialize(car);
+        }
+    }
+}

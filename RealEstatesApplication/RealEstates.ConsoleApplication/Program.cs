2d6990c21c6bcb4f212485edcc6b17d9984b202a
Milen Dinev <dinev.milen@gmail.com>
2021-04-02 16:58:31 +0300

    Initial Commit

---
+++ RealEstatesApplication/RealEstates.ConsoleApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace RealEstates.ConsoleApplication
+{
+    using Microsoft.EntityFrameworkCore;
+    using RealEstates.Data;
+    using RealEstates.Models;
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new ApplicationDbContext();
+            db.Database.Migrate();
+
+            db.Districts.Add(new District
+            {
+                Name = "Дианабад"
+
+            });
+
+            db.SaveChanges();
+
+        }
+    }
+}
---
+++ RealEstatesApplication/RealEstates.Models/District.cs
@@ -0,0 +1,18 @@
+﻿namespace RealEstates.Models
+{
+    using System.Collections.Generic;
+
+    public class District
+    {
+
+        public District()
+        {
+            this.Properties = new HashSet<Property>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Property> Properties { get; set; }
+    }
+}

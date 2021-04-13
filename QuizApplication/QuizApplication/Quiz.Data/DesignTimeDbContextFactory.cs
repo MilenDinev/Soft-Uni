560056697628ab791cf2bf6f727d6c96cd33dc75
Milen Dinev <dinev.milen@gmail.com>
2021-04-13 17:26:36 +0300

    Database configuration was implemented

--- QuizApplication/QuizApplication/Quiz.Data/DesignTimeDbContextFactory.cs
+++ QuizApplication/QuizApplication/Quiz.Data/DesignTimeDbContextFactory.cs
@@ -1,6 +1,24 @@
-﻿namespace Quiz.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Quiz.Data
 {
-    public class DesignTimeDbContextFactory
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
     {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+
+
+        }
     }
 }

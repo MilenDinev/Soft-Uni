6cf7b188ea8b12e638cab5a261c45d2b6742b895
Milen Dinev <dinev.milen@gmail.com>
2021-05-23 11:48:42 +0300

    tests tasks

---
+++ Web Module/02.WEB SERVER - ASYNCHRONOUS PROCESSING/Tasks/EvenNumbersThread/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace EvenNumbersThread
+{
+    using System;
+    using System.Threading;
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+            Thread evens = new Thread(() =>
+                PrintEvenNumbers(start, end));
+            evens.Start();
+
+            evens.Join();
+            Console.WriteLine("Thread finished work");
+
+        }
+
+        static void PrintEvenNumbers(int a, int b)
+        {
+            for (int i = a; i <= b; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
---
+++ Web Module/02.WEB SERVER - ASYNCHRONOUS PROCESSING/Tasks/Tests/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            PrintNumbersInRange(0, 100);
+
+            var task = Task.Run(() =>
+                PrintNumbersInRange(100, 200));
+
+            Console.WriteLine("Done.");
+            task.Wait();
+
+            Thread primes = new Thread(() => PrintNumbersInRange(1, 50));
+            primes.Start();
+            Console.WriteLine("Waiting for thread to finish work...");
+            primes.Join();
+
+        }
+
+        static void PrintNumbersInRange(int a, int b)
+        {
+            for (int i = a; i <= b; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}

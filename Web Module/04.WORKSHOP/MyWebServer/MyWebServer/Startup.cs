0321a67b656048c7eaebaa7def4fea8f56659a6b
Milen Dinev <dinev.milen@gmail.com>
2021-06-01 21:10:37 +0300

    Created web server and implemented browser connectivity

---
+++ Web Module/04.WORKSHOP/MyWebServer/MyWebServer/Startup.cs
@@ -0,0 +1,20 @@
+﻿namespace MyWebServer
+{
+    using System;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;
+    using System.Threading.Tasks;
+    using MyWebServer.Server;
+    //localhost 127.0.0.1
+
+    public class Startup
+    {
+        static async Task Main(string[] args)
+        {
+            var server = new HttpServer("127.0.0.1", 8080);
+
+            await server.Start();
+        }
+    }
+}
